39038832aac1459c22f63f030babcb0e9a56a996
cesar maciel <cesarjaviermaciel@gmail.com>
2026-03-05 20:46:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9649908979-71f28",
+  "appId": "1:687384587101:web:e45ea2ab87ddef6b9456b4",
+  "apiKey": "AIzaSyBj3STFfRB0nkaW6a6njU4Yl7geYkTzx4A",
+  "authDomain": "studio-9649908979-71f28.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "687384587101"
+};
